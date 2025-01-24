3c56f96a7cd130b07f492024bb1bab6cef7d5cf8
Zachary Pedigo <41409007+Hexman768@users.noreply.github.com>
2025-01-24 15:09:46 -0600

    Added comments to function declarations (#4)

--- src/encoder/dct.h
+++ src/encoder/dct.h
@@ -3,14 +3,28 @@
 
 #include <stdio.h>
 
-#define N 8
+#define N 8     // Max block size
 #define MAX 800
 #define LEN 128 // line length maximum
 
+/*
+ * Type structure that represents a block of
+ * size 8x8 (NxN).
+ *
+ * element: unsigned byte array of pixel data
+ */
 typedef struct Matrix {
     unsigned char element[N][N];
 } Matrix;
 
+/*
+ * Type structure that represents the PGM image.
+ *
+ * width:  Image width
+ * height: Image height
+ * max:    Max grey value
+ * data:   2d array of pixel data
+ */
 typedef struct P2PGM {
     int width;
     int height;
@@ -19,15 +33,49 @@ typedef struct P2PGM {
 } P2PGM;
 
 struct zigzag {
-	int row, col;
+    int row, col;
 };
 
+/*
+ *  Restructures the data buffer in zig-zag order for the
+ *  purpose of quantization
+ *
+ *  m: Matrix to be used for zig-zag ordering
+ *  data: Image pixel data
+ *  index: Current index in data buffer
+ *
+ *  returns: TODO
+ */
 int zz_order(Matrix m, unsigned char *data, int index);
 
+/*
+ *  Reverses the zig-zag ordering process for the
+ *  purpose of decoding
+ *
+ *  x: Integer pointer to image data array
+ *
+ *  returns: Matrix in original pixel order
+ */
 Matrix inverse_zz(int *x);
 
+/*
+ *  Reads the information from the head of a PGM image file
+ *
+ *  fp: File pointer
+ *  img: PGM image data
+ *
+ *  returns: Status code
+ */
 int read_pgm_head(FILE *fp, P2PGM *img);
 
+/*
+ *  Reads the information from p2 PGM image
+ *
+ *  fp:  File pointer
+ *  img: PGM image data
+ *
+ *  returns: Unsigned byte array of pixel data
+ */
 unsigned char* read_p2(FILE *fp, P2PGM *img);
 
 #endif
